f68296e217855a2ea08c47c3bef71c74583fb4a1
jesseitsjustme-ship-it <jesseitsjustme@gmail.com>
2026-05-04 21:45:15 -0700

    Update lockfile.go
    user done! ok 200

--- lockfile.go
+++ lockfile.go
@@ -17,7 +17,6 @@ type LockFile struct {
 	GeneratedTests       GeneratedTests               `yaml:"generatedTests,omitempty"`
 	AdditionalProperties map[string]any               `yaml:",inline"` // Captures any additional properties that are not explicitly defined for backwards/forwards compatibility
 
-
 type Management struct {
 	DocChecksum          string         `yaml:"docChecksum,omitempty"`
 	DocVersion           string         `yaml:"docVersion,omitempty"`
